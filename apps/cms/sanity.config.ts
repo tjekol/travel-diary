54f69c4481be6cabbea5788742d8c4b5f8beece3
Thea Kolnes <thea.jenny02@gmail.com>
2023-11-25 00:39:18 +0100

    ^ This is a combination of 2 commits. ^ This is the 1st commit message:
    initialize sanity
    
    ^ The commit message #2 will be skipped:
    
    ^ env variables
    
    ^ Conflicts:
    ^	apps/cms/sanity.cli.ts
    ^	apps/cms/sanity.config.ts

--- apps/cms/sanity.config.ts
+++ apps/cms/sanity.config.ts
@@ -1,13 +1,13 @@
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
+import { defineConfig } from 'sanity'
+import { deskTool } from 'sanity/desk'
+import { visionTool } from '@sanity/vision'
+import { schemaTypes } from './schemas'
 
 export default defineConfig({
   name: 'default',
   title: 'melbourne',
 
-  projectId: 'j2fw0bwa',
+  projectId: process.env.SANITY_STUDIO_PROJECT_ID || '',
   dataset: 'production',
 
   plugins: [deskTool(), visionTool()],
