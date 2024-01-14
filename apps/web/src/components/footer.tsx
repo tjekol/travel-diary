6bf6316a1db0f4e51831faa5cecb06e627882976
Thea Kolnes <thea.jenny02@gmail.com>
2024-01-14 18:37:46 +0100

    github log in and out, footer, admin page

---
+++ apps/web/src/components/footer.tsx
@@ -0,0 +1,5 @@
+export default function Footer() {
+  return (
+    <a href='https://github.com/tjekol/melbourne' target='_' className='static hover:underline self-center bottom-2 text-text/50 mt-8'>Thea Jenny E. Kolnes🦋</a>
+  );
+}
