d71ecfc5ab23863cdc92e442f21a8e006b1c3415
Thea Kolnes <thea.jenny02@gmail.com>
2024-01-14 18:37:46 +0100

    github log in and out, footer, admin page

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,4 +1,7 @@
+import { signOut, useSession } from 'next-auth/react';
+
 export default function Header() {
+  const session = useSession();
   return (
       <div className='self-center my-8'>
         <a href='/'>
@@ -9,9 +12,18 @@ export default function Header() {
             Follow my journey in Melbourne
           </h3>
         </a>
-        <a href='http://localhost:3000/api/auth/signin/github'>
-          <p className='absolute hover:underline top-5 right-5 sm:top-10 sm:right-10 text-text/50 justify-self-end'>Logg inn</p>
-        </a>
+        {session.status === 'authenticated' ? (
+          <div className='flex flex-col items-end absolute top-5 right-5 sm:top-10 sm:right-10'>
+            <a href='http://localhost:3000/admin'>
+              <p className='text-text/50 hover:underline'>Admin</p>
+            </a>
+            <p onClick={() => signOut()} className='text-text/50 hover:underline'>Logg ut</p>
+          </div>
+        ) : (
+          <a href='http://localhost:3000/api/auth/signin/github'>
+            <p className='absolute hover:underline top-5 right-5 sm:top-10 sm:right-10 text-text/50 justify-self-end'>Logg inn</p>
+          </a>
+        )}
       </div>
   );
 }
