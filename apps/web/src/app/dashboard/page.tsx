867c026beb9eee7d335c21f6e63dd994abf5a842
Thea Kolnes <thea.jenny02@gmail.com>
2024-01-14 20:43:04 +0100

    upt to dashboard, google sign in

---
+++ apps/web/src/app/dashboard/page.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import Footer from '@/components/footer';
+import Header from '@/components/header';
+import { useSession } from 'next-auth/react';
+
+export default function Admin() {
+  const { data } = useSession();
+  const session = useSession();
+
+  return (
+    <main className='flex min-h-screen flex-col justify-between p-6 sm:p-12'>
+      <Header />
+      {session.status === 'authenticated' ? (
+        <>
+          <p>authenticated</p>
+          <p>Admin {JSON.stringify(data)}</p>
+        </>
+      ) : (
+        <>
+          <div className='self-center rounded-md bg-secondary px-10 py-4 hover:bg-secondary/40'>
+            <a href='http://localhost:3000/api/auth/signin/github'>
+              <h1 className='font-semibold'>Logg inn</h1>
+            </a>
+          </div>
+          <p />
+        </>
+      )}
+      <Footer />
+    </main>
+  );
+}
--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,21 +1,25 @@
+'use client';
+
 import { signOut, useSession } from 'next-auth/react';
 
 export default function Header() {
   const session = useSession();
+
+
   return (
       <div className='self-center my-8'>
         <a href='/'>
           <h1 className='font-serif font-semibold text-center'>
-            Melbourne Diary🇦🇺
+            Melbourne Dagbok🇦🇺
           </h1>
           <h3 className='text-center text-text/60'>
-            Follow my journey in Melbourne
+            Følg min reise til Melbourne
           </h3>
         </a>
         {session.status === 'authenticated' ? (
           <div className='flex flex-col items-end absolute top-5 right-5 sm:top-10 sm:right-10'>
-            <a href='http://localhost:3000/admin'>
-              <p className='text-text/50 hover:underline'>Admin</p>
+            <a href='http://localhost:3000/dashboard'>
+              <p className='text-text/50 hover:underline'>Hei, {session.data.user?.name}!</p>
             </a>
             <p onClick={() => signOut()} className='text-text/50 hover:underline'>Logg ut</p>
           </div>
