19b9e5110e0878866dd38be2fdc843e8e3219e52
Thea Kolnes <thea.jenny02@gmail.com>
2024-01-21 01:21:35 +0100

    removed log in

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -7,17 +7,29 @@ import { IPost } from '@/sanity/post/schemas';
 import Image from 'next/image';
 import { useState } from 'react';
 import { useEffect } from 'react';
+import { PulseLoader } from 'react-spinners';
 
 export default function Home() {
   const [posts, setPosts] = useState<IPost[]>([]);
+  const [loadingImages, setLoadingImages] = useState<string[]>([]);
   
   useEffect(() => {
     const fetchPosts = async () => {
-      const posts = await getPosts();
-      setPosts(posts);
+      try {
+        const posts = await getPosts();
+        setPosts(posts);
+      } catch (err) {
+        console.error(err);
+      }
     };
     fetchPosts();
   }, [posts]);
+
+  const handleImageLoad = (postId: string) => {
+    setLoadingImages((prevLoadingImages) =>
+      prevLoadingImages.filter((id) => id !== postId)
+    );
+  };
 
   return (
     <main className='flex min-h-screen flex-col justify-between p-6 sm:p-12'>
@@ -25,10 +37,13 @@ export default function Home() {
       <div className='w-full md:w-5/6 self-center justify-items-center grid grid-cols-2 xl:grid-cols-4 gap-3 sm:gap-4'>
         {posts.map((post) => (
           <div key={post._id} className='hover:scale-105 bg-primary/70 p-1 sm:p-2 rounded-sm'>
+            {loadingImages.includes(post._id) && (
+              <PulseLoader size={10} />
+            )}
             <a href={'post/' + post.slug.current}>
-              <Image className='aspect-square object-cover' src={post.mainImage} alt={post.title} width={400} height={400} />
-              <p className='text-text'>{post.title}</p>
-              <p className='text-accent font-light'>{post.publishedAt}</p>
+            <Image className='aspect-square object-cover' onLoad={() => handleImageLoad(post._id)} placeholder='empty' loading='lazy' src={post.mainImage} alt={post.title} width={400} height={400} />
+            <p className='text-text'>{post.title}</p>
+            <p className='text-accent font-light'>{post.publishedAt}</p>
             </a>
           </div>
         ))}
