78701a8a7d476c94544ba8bf1e4f8a9f6430f4a2
Thea Jenny E Kolnes <thea.jenny02@gmail.com>
2025-08-05 19:38:43 +0200

    chore: update dependencies
    Updated dependencies as well as next.js and tailwindCSS

--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -2,26 +2,8 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-      colors: {
-        "text": '#1d100c',
-        "background": '#F6EDEA',
-        "primary": '#bf7763',
-        "secondary": '#e1c0b7',
-        "accent": '#914e3b',
-      }
-    },
-  },
   plugins: [],
 };
 export default config;
