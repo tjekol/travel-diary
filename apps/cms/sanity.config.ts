400271b814535d5f3fcc5a35a3c32649e2bf54ff
Thea Jenny E Kolnes <thea.jenny02@gmail.com>
2025-08-05 22:04:21 +0200

    feat: travel/trip seperator

--- apps/cms/sanity.config.ts
+++ apps/cms/sanity.config.ts
@@ -5,7 +5,7 @@ import {schemaTypes} from './schemas'
 
 export default defineConfig({
   name: 'default',
-  title: 'melbourne',
+  title: 'travel-diary',
 
   projectId: 'j2fw0bwa',
   dataset: 'production',
